4a3d79395b5b7f67f90dbd9a50535d02318bcc1f
Becher <mbelkhenchir@gmail.com>
2020-03-31 16:11:26 +0200

    Distorsion du sol a cause de la chaleur

--- Assets/Scripts/Tools/GizmosSun.cs
+++ Assets/Scripts/Tools/GizmosSun.cs
@@ -1,14 +1,17 @@
-﻿#if UNITY_EDITOR
+﻿
 
 using System;
 using System.Collections;
 using System.Collections.Generic;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 using UnityEngine;
 using UnityEngine.Experimental.GlobalIllumination;
 [ExecuteInEditMode]
 public class GizmosSun : MonoBehaviour
 {
+#if UNITY_EDITOR
     private LightController _light;
     [SerializeField] private Vector2 center= new Vector2(80,80);
     private Vector2 actualCenter;
@@ -215,6 +218,5 @@ public class GizmosSun : MonoBehaviour
     {
         return RotatePos(angle, radius);
     }
-}
-
 #endif
+}
