e98f6921b7e5125ca80cd6ca87a27bca4624f821
kinaar <guerric.chab@gmail.com>
2020-04-01 18:07:08 +0200

    Soundbank Updated
    Music + Amb + Footsteps + Astro + Impacts Soleil

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,8 +13,19 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID PLAY_SOUNDTEST = 3429081479U;
+        static const AkUniqueID AMB_DESERT_PLAY = 2160078966U;
+        static const AkUniqueID ASTRO_MOVING_PLAY = 3822072736U;
+        static const AkUniqueID ASTRO_MOVING_STOP = 2165331746U;
+        static const AkUniqueID CHA_FOOTSTEPS_PLAY = 1343142182U;
+        static const AkUniqueID MUSIC_PLAY = 202194903U;
+        static const AkUniqueID SUN_FEEDBACK_PLAY = 2337456342U;
     } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID RTPC_DISTANCE_SUN = 2257099397U;
+        static const AkUniqueID RTPC_WINDGAIN = 1687410496U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
