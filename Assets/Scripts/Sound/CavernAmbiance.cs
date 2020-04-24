38855424412e4c0d210bc59e652faa776d282f46
Becher <mbelkhenchir@gmail.com>
2020-04-24 15:23:55 +0200

    Clean some assets

--- Assets/Scripts/Sound/CavernAmbiance.cs
+++ Assets/Scripts/Sound/CavernAmbiance.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.SceneManagement;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CavernAmbiance : MonoBehaviour
 {
