e3dfa78cfebac27351d630d11f9d776ac2c33a77
Becher <mbelkhenchir@gmail.com>
2020-06-18 00:06:15 +0200

    End Game

--- Assets/Scripts/End/Anim.cs
+++ Assets/Scripts/End/Anim.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Anim : MonoBehaviour
 {
@@ -9,7 +10,41 @@ public class Anim : MonoBehaviour
     public Watcher watcher;
     public Door door;
     private bool open;
+    public Animation cmcam;
     
+    public CanvasGroup ui;
+    public CanvasGroup background;
+    public float uiTimer;
+    public AnimationCurve uiCurve;
+
+
+    public float restart;
+    void Update()
+    {
+        if (open)
+        {
+            uiTimer -= Time.deltaTime;
+            if (uiTimer < 0) restart = 5;
+            if(uiTimer < 5 && uiTimer >= 2)
+            {
+                ui.alpha = uiCurve.Evaluate(1 - (uiTimer - 2) / 3);
+            }
+            else if (uiTimer < 2) background.alpha = 1;
+            
+            if (uiTimer < 3 && uiTimer >= 1)
+            {
+                background.alpha = 1 - (uiTimer - 1) / 2;
+            }
+            else if (uiTimer < 1)
+            {
+                background.alpha = 1;
+                ui.interactable = true;
+                ui.blocksRaycasts = true;
+            }
+
+
+        }
+    }
     void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.gameObject.name);
@@ -22,9 +57,19 @@ public class Anim : MonoBehaviour
     {
         if (!open)
         {
+            cmcam.Play();
             door.Open();
             open = true;
-            
+            uiTimer = 10;
         }
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
     }
 }
